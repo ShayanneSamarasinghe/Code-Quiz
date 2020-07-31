2192a455803a4551a3f8fb4c11a3a9bc4b7b0786
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-31 10:43:29 -0400

    Restarted. Followed walkthrough

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-/*
-        GIVEN I am taking a code quiz
-        WHEN I click the start button
-        THEN a timer starts and I am presented with a question
-        WHEN I answer a question
-        THEN I am presented with another question
-        WHEN I answer a question incorrectly
-        THEN time is subtracted from the clock
-        WHEN all questions are answered or the timer reaches 0
-        THEN the game is over
-        WHEN the game is over
-        THEN I can save my initials and score
-        */
-
     const questions = [
         { 
           question: "What colour is the sky?",
@@ -44,23 +30,23 @@
 
     function checkTime(){
       timer = timer - 1;
-      document.querySelector('#timer').innerHTML = `${timer} seconds remaining...`;
+      document.querySelector('#timer').innerHTML = `Countdown: ${timer}` ;
   
       if( timer<1 ){
-          // time is up! end the game
+          
           showFinished();
       }
   }
 
   function showQuestion(){
     const currentQuestion = questions[questionNum];
-    document.querySelector('#questions').innerHTML = currentQuestion.text;
+    document.querySelector('#question').innerHTML = currentQuestion.text;
 
     document.querySelector('#answers').innerHTML = '';
     for( var i=0; i<currentQuestion.answers.length; i++ ){
         const activeAnswer = currentQuestion.answers[i];
         document.querySelector('#answers').innerHTML += 
-            `<button onClick="checkAnswer(event,'${activeAnswer}')" class="list-group-item">${activeAnswer}</button>`
+        `<li onClick="checkAnswer(event,'${activeAnswer}')" class="list-group-item">${activeAnswer}</li>`
     }
 }
 
@@ -69,9 +55,9 @@ function checkAnswer( event, userAnswer ){
 
     console.log( `[checkAnswer] checking question, answer given: ${userAnswer}` );
     
-    // check if user got hte question right, 
+
     const currentQuestion = questions[questionNum];
-    // display a message            
+         
     if( currentQuestion.correctAnswer === userAnswer ){
         document.querySelector('#alertCorrect').style.display = 'block';
         document.querySelector('#alertWrong').style.display = 'none';
@@ -82,38 +68,34 @@ function checkAnswer( event, userAnswer ){
         statsWrongAnswers++;
     }
 
-    // move on to next question (if any more exist)
+   
     questionNum = questionNum+1;
     if( questionNum >= questions.length ){
-        // we are done! no more questions!
+        
         showFinished();
         return;
     };
 
-    // show next question
+    
     showQuestion();
 }
 
 function showFinished(){
-  // show whatever code for the finished display here
   document.querySelector('#alertCorrect').style.display = 'block';
   document.querySelector('#alertCorrect').innerHTML =  
       `You got this many right: ${statsCorrectAnswers}, and this many wrong: ${statsWrongAnswers}`;
-  // stop the timer
+
   clearInterval( timerInterval );
 }
-
-
-
-
-
 
 
 function startGame(){
  timer = 200;
  timerInterval = setInterval( checkTime, 1000 );
 
-  questionNum = 0;
-  showQuestion();
-  
-  }
+ questionNum = 0;
+ showQuestion();
+
+}
+
+startGame();
